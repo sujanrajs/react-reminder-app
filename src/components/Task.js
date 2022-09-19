4783653513ce1816d7f247dfd58dd99b5613b8e5
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-09-19 20:19:47 +0300

    install react icons and add delete icon on task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,12 @@
+import { MdCancel } from "react-icons/md";
+
 const Task = ({ task }) => {
   return (
     <div>
-      <h3>{task.title}</h3>
+      <h3>
+        {task.title}
+        <MdCancel />
+      </h3>
       <p>{task.day}</p>
     </div>
   );
