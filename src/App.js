cf800c256b3ea205f8af2a6b8f54b62fdb839478
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-09-19 19:58:46 +0300

    add tasks component and db.json file, install json server, fetch data from db.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+
   return (
     <div>
       <Header title="Reminder App" />
+      <Tasks tasks={tasks} />
     </div>
   );
 };
